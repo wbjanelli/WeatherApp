537ccee310aa1d95eadee67bbaaea052a0366ee4
William Janelli <williamjanelli@yahoo.com>
2024-05-02 15:48:32 -0700

    WeatherUpdate

--- src/Components/BackgroundLayout.jsx
+++ src/Components/BackgroundLayout.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useStateContext } from '../Context'
 //images
 import Clear from '../assets/images/Clear.jpg'
-import Fog from '../assets/images/fog.png'
+import Fog from '../assets/images/fog.jpg'
 import Cloudy from '../assets/images/Cloudy.jpg'
 import Rainy from '../assets/images/Rainy.jpg'
 import Snow from '../assets/images/snow.jpg'
